9624f7771facca08615fa8a80da3ce2a4a47e71d
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-08-11 12:32:11 +0200

    Main is cooler

--- excelreader/src/main/java/net/aequologica/sangiovannilipioni/Main.java
+++ excelreader/src/main/java/net/aequologica/sangiovannilipioni/Main.java
@@ -22,7 +22,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class App {
+public class Main {
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
         String dir = "../files/";
         String filename = "Sintesi O2";
